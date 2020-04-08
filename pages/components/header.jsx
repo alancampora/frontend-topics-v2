10d9554a1121b588eb3e24c21fc10d37fe409b7e
Alan Campora <alan.campora@gmail.com>
2020-04-07 23:53:00 -0300

    refactor(front): first structure

--- pages/components/header.jsx
+++ pages/components/header.jsx
@@ -1,11 +1,56 @@
 export default function Header() {
 	return (
-		<div className="header p-4">
-			<img
-				className="w-50 shadow-lg rounded-full mx-auto border-solid border-2 border-gray-300"
-				src="/profile.jpeg"
-			/>
-          <h1 className="text-2xl text-center font-mono p-2">{`<Frontend Topics />`}</h1>
-		</div>
+		<nav className="flex items-center flex-wrap bg-gray-800 py-2 px-4 justify-between">
+			<div className="flex items-center flex-shrink-0 text-white mr-6">
+				<a className="text-white no-underline hover:text-white hover:no-underline">
+					<span className="text-xl font-mono">{`<Frontend Topics />`}</span>
+				</a>
+			</div>
+
+			<div class="block lg:hidden">
+				<button
+					id="nav-toggle"
+					class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white"
+				>
+					<svg
+						class="fill-current h-3 w-3"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+					</svg>
+				</button>
+			</div>
+
+			<div class="flex-grow lg:flex lg:items-center lg:w-auto lg:block w-full">
+				<ul class="list-reset lg:flex justify-end flex-1 items-center">
+					<li class="mr-3">
+						<a class="inline-block py-2 px-4 text-white no-underline" href="#">
+							Active
+						</a>
+					</li>
+					<li class="mr-3">
+						<a
+							class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+							href="#"
+						>
+							link
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
 	);
 }
+
+//<div className="header p-4">
+//<div className="flex items-center">
+//<div>
+//<img
+//className="w-10 shadow-lg rounded-full border-solid border-2 border-gray-300 flex-shrink-0"
+//src="/profile.jpeg"
+///>
+//<h1 className="text-xl text-center font-mono p-2">{`<Frontend Topics />`}</h1>
+//</div>
+//</div>
+//</div>
--- pages/index.js
+++ pages/index.js
@@ -7,23 +7,26 @@ import Navbar from './components/navbar';
 
 export default function Index({ playlists }) {
 	return (
-		<div className="container mx-auto p-4">
+		<div>
 			<Header />
-			<div className="container mx-auto">
-				{playlists &&
-					playlists.items &&
-					playlists.items.map((element, index) => {
-						return (
-							<div className="container my-10">
-								<div className="flex items-center">
-                  <div className="
+			<div className="container mx-auto p-4">
+				<div className="container mx-auto">
+					{playlists &&
+						playlists.items &&
+						playlists.items.map((element, index) => {
+							return (
+								<div className="container my-10">
+									<div className="flex items-center">
+										<div
+											className="
                       h-p 
                       border-t-2 
                       border-indigo-100
                       w-full
-                    "></div>
-									<div
-										className="
+                    "
+										></div>
+										<div
+											className="
                       flex-shrink-0
                       text-center 
                       text-black 
@@ -32,30 +35,36 @@ export default function Index({ playlists }) {
                       text-xl
                       rounded-md
                       bg-indigo-100
-                    ">
-										{element.title}
-									</div>
-                    <div className="
+                    "
+										>
+											{element.title}
+										</div>
+										<div
+											className="
                       h-p 
                       border-t-2 
                       border-indigo-100
                       w-full
-                    "></div>
-								</div>
-                  <div className="
+                    "
+										></div>
+									</div>
+									<div
+										className="
                     py-2 
                     border-grey-500 
                     rounded-lg
-                  ">
-									{element.videos.map(video => (
-										<Card {...video} />
-									))}
+                  "
+									>
+										{element.videos.map(video => (
+											<Card {...video} />
+										))}
+									</div>
 								</div>
-							</div>
-						);
-					})}
+							);
+						})}
+				</div>
+				<Footer />
 			</div>
-			<Footer />
 		</div>
 	);
 }
