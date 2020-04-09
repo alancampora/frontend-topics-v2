f59b82bd33213625a6f02c469ab49ba097e59ba2
Alan Campora <alan.campora@gmail.com>
2020-04-09 02:02:33 -0300

    refactor(front): hero font

--- pages/components/hero.jsx
+++ pages/components/hero.jsx
@@ -3,7 +3,7 @@ export default function Hero() {
 		<div className="h-200 bg-purple-500 text-yellow-200">
 			<div className="flex mx-auto p-5 items-center max-w-5xl">
 				<div className="flex-1 text-right p-4">
-					<div className="font-serif text-4xl w-25">
+					<div className="font-hero text-4xl w-25">
 						Learning the latest Frontend frameworks for free. Javascript, HTML,
 						CSS and more!
 					</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,8 @@ module.exports = {
 		container: {
 			center: false,
 		},
-		//colors: {
-			//main: {
-				//dark: '#21232b',
-				//semiDark: '#292c34',
-			//},
-		//},
+		fontFamily: {
+			hero: ['Nunito', 'sans-serif'],
+		},
 	},
 };
