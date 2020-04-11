387c4626d16a459c73945e3bb8dc7501d4511161
Alan Campora <alan.campora@gmail.com>
2020-04-11 17:49:00 -0300

    refactor(back): dynamic routing for all-videos (#5)
    * refactor(back): dynamic routing for all-videos
    
    * refactor(*): introduce change after deploying

--- pages/api/en/all-videos.js
+++
@@ -1,9 +0,0 @@
-import getAllVideosByPlaylist from '../_utils/all-videos';
-
-export default async function get(req, res) {
-
-	const key = process.env.YOUTUBE_KEY;
-	const channelId = process.env.YOUTUBE_CHANNEL_EN;
-
-	return await getAllVideosByPlaylist(key, channelId)(req, res);
-}
