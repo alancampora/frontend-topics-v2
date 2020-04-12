3fa180dd3df692df291f557f10dd1fcfda8e2abc
Alan Campora <alan.campora@gmail.com>
2020-04-12 18:29:37 -0300

    feat(blog): blog list

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,34 +3,46 @@ import Link from 'next/link';
 import Layout from '../../components/layout';
 
 export default function Blog(props) {
-  return (
-    <Layout>
-      {props.posts.map((post, index) => (
-        <div key={index}>
-          <Link href={post.url}>
-            <a>{post.title}</a>
-          </Link>
-        </div>
-      ))}
-    </Layout>
-  );
+	return (
+		<Layout>
+			<div className="flex flex-wrap justify-center">
+				{props.posts.map((post, index) => (
+					<div className="m-4" key={index}>
+						<Link href={post.url}>
+							<div className="max-w-sm rounded shadow-lg">
+								<img
+									className="w-full"
+									src={post.hero.image}
+									alt="Sunset in the mountains"
+								/>
+								<div className="px-6 py-4">
+									<div className="font-bold text-xl mb-2">{post.title}</div>
+									<p className="text-gray-700 text-base">{post.description}</p>
+								</div>
+							</div>
+						</Link>
+					</div>
+				))}
+			</div>
+		</Layout>
+	);
 }
 
 export async function getStaticProps() {
-  const slugs = glob
-    .sync('**/p/**/*.mdx')
-    .map(slug => slug.replace(/.*\//, '').replace(/\.mdx/, ''));
+	const slugs = glob
+		.sync('**/p/**/*.mdx')
+		.map(slug => slug.replace(/.*\//, '').replace(/\.mdx/, ''));
 
-  const posts = await Promise.all(
-    slugs.map(async slug => {
-      const file = await import(`./p/${slug}.mdx`);
-      return { ...file.frontmatter, url: `/blog/p/${slug}` };
-    })
-  );
+	const posts = await Promise.all(
+		slugs.map(async slug => {
+			const file = await import(`./p/${slug}.mdx`);
+			return { ...file.frontmatter, url: `/blog/p/${slug}` };
+		}),
+	);
 
-  return {
-    props: {
-      posts: posts
-    }
-  };
+	return {
+		props: {
+			posts: posts,
+		},
+	};
 }
