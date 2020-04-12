df503d4dc82f373717bfa3248d2886bb0380507d
Alan Campora <alan.campora@gmail.com>
2020-04-11 22:22:29 -0300

    feat(blog): integration with mdx

---
+++ pages/blog/index.js
@@ -0,0 +1,32 @@
+import glob from 'glob';
+import Link from 'next/link';
+
+export default function Blog(props) {
+	return props.posts.map(post => (
+		<div>
+			{post.title}
+			<Link href={post.url}>
+				<a>{post.title}</a>
+			</Link>
+		</div>
+	));
+}
+
+export async function getStaticProps() {
+	const slugs = glob
+		.sync('**/p/**/*.mdx')
+		.map(slug => slug.replace(/.*\//, '').replace(/\.mdx/, ''));
+
+	const posts = await Promise.all(
+		slugs.map(async slug => {
+			const file = await import(`./p/${slug}.mdx`);
+			return { ...file.frontmatter, url: `/blog/p/${slug}` };
+		}),
+	);
+
+	return {
+		props: {
+			posts: posts,
+		},
+	};
+}
