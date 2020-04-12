531deb0a57eb7b011f81a95e614b9e91d4d8e936
Alan Campora <alan.campora@gmail.com>
2020-04-12 03:20:54 -0300

    feat(blog): custom styles for mdx

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,32 +1,36 @@
 import glob from 'glob';
 import Link from 'next/link';
+import Layout from '../components/layout';
 
 export default function Blog(props) {
-	return props.posts.map(post => (
-		<div>
-			{post.title}
-			<Link href={post.url}>
-				<a>{post.title}</a>
-			</Link>
-		</div>
-	));
+  return (
+    <Layout>
+      {props.posts.map((post, index) => (
+        <div key={index}>
+          <Link href={post.url}>
+            <a>{post.title}</a>
+          </Link>
+        </div>
+      ))}
+    </Layout>
+  );
 }
 
 export async function getStaticProps() {
-	const slugs = glob
-		.sync('**/p/**/*.mdx')
-		.map(slug => slug.replace(/.*\//, '').replace(/\.mdx/, ''));
+  const slugs = glob
+    .sync('**/p/**/*.mdx')
+    .map(slug => slug.replace(/.*\//, '').replace(/\.mdx/, ''));
 
-	const posts = await Promise.all(
-		slugs.map(async slug => {
-			const file = await import(`./p/${slug}.mdx`);
-			return { ...file.frontmatter, url: `/blog/p/${slug}` };
-		}),
-	);
+  const posts = await Promise.all(
+    slugs.map(async slug => {
+      const file = await import(`./p/${slug}.mdx`);
+      return { ...file.frontmatter, url: `/blog/p/${slug}` };
+    })
+  );
 
-	return {
-		props: {
-			posts: posts,
-		},
-	};
+  return {
+    props: {
+      posts: posts
+    }
+  };
 }
